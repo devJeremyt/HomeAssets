c85999717b72f85c15e8c1d7a47f95831cc3ffcd
Jeremy Trimble <devjeremyt@gmail.com>
2022-01-30 21:21:37 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeAssets.Startup))]
+namespace HomeAssets
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
